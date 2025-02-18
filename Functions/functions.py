a13c725fb8c691da97adc991ba19915b8008e7c6
Julian <ludicolo2209@gmail.com>
2025-02-17 21:01:11 -0500

    First test

--- Functions/functions.py
+++ Functions/functions.py
@@ -3,9 +3,12 @@ class Main:
     def __init__(self, a, b):
         self.a = a
         self.b = b
+        self.sum = 0
         
     
     def suma(self, x, y):
-       sum = self.a + self.b + x + y
+       self.sum += self.a + self.b + x + y
        
-       return sum
+   
+    def imprimir(self):
+       return print(f"{self.sum}")
--- main.py
+++ main.py
@@ -3,9 +3,10 @@ from Functions.functions import *
 
 def main():
     suma = Main(1,2)
-    x = suma.suma(2, 2)
-    print(x)
-
+    suma.suma(2, 2)
+    suma.imprimir()
+    suma.suma(2, 2)
+    suma.imprimir()
 
 if __name__ == "__main__":
     main()
