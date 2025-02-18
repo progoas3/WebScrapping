630eb70ec51480087f017cb6cc4b5740b1d9af68
Julian <ludicolo2209@gmail.com>
2025-02-17 20:47:52 -0500

    First test

---
+++ Functions/functions.py
@@ -0,0 +1,11 @@
+class Main:
+    
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+        
+    
+    def suma(self, x, y):
+       sum = self.a + self.b + x + y
+       
+       return sum
--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-print("Hola mundo")
-print("zzzz")
+from Functions.functions import *
+
+
+def main():
+    suma = Main(1,2)
+    x = suma.suma(2, 2)
+    print(x)
+
+
+if __name__ == "__main__":
+    main()
