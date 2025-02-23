8d874157db9eaf3d1ea21318f0bf963b359ef9f6
Julian <ludicolo2209@gmail.com>
2025-02-23 17:58:12 -0500

    Deployed first WebScrapping

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from Functions.functions import *
 
 
 def main():
-    connection = Main("https://escueladirecta-blog.blogspot.com/", "h3")
+    connection = Main("https://www.mercadolibre.com.co/ofertas#nav-header",".poly-component__title", ".andes-money-amount__fraction", 
+                      "span.andes-money-amount.andes-money-amount--cents-superscript > span.andes-money-amount__fraction")
     connection.extract()
 
 if __name__ == "__main__":
